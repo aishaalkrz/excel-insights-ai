2d6b63767a01492902a6e3527eb69f4fc0d5bdab
aishaalkrz <aishakrez@gmail.com>
2026-06-14 15:45:08 +0300

    update

--- excel-insights-api/main.py
+++ excel-insights-api/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Excel Insights API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:4200",
-    "https://basira-analytics.netlify.app/"],
+    "https://basira-analytics.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
